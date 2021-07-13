85ffd2f215bac49cf591599c62239334738570f0
Evan Mead <evan.c.mead@gmail.com>
2021-07-12 19:15:53 -0500

    init commit

---
+++ src/main/java/com/example/humanesociety/HumaneSocietyApplication.java
@@ -0,0 +1,13 @@
+package com.example.humanesociety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumaneSocietyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumaneSocietyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/humanesociety/HumaneSocietyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.humanesociety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumaneSocietyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
